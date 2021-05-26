34529aac5f518a6f30c187c754e983cd07533de5
Andrew Huth <andrew@huth.me>
2021-05-26 07:11:38 -0700

    Go back to "tick"-ing the interpreter, instead of using a generator
    There is not really a need for the generator. So lets simplify things.

--- src/emulator/Interpreter.test.ts
+++ src/emulator/Interpreter.test.ts
@@ -2,13 +2,13 @@ import Interpreter from './Interpreter';
 
 test('interpreting', () => {
   const interpreter = new Interpreter();
-  const gen = interpreter.interpret([
+  interpreter.load([
     // Clear screen.
     0x00, 0xE0,
     // Put 0xAB into register V1.
     0x61, 0xAB,
   ]);
-  expect(gen.next().value).toEqual(0x00E0);
-  expect(gen.next().value).toEqual(0x61AB);
-  expect(gen.next().done).toEqual(true);
+  expect(interpreter.tick()).toEqual(true);
+  expect(interpreter.tick()).toEqual(true);
+  expect(interpreter.tick()).toEqual(false);
 });
