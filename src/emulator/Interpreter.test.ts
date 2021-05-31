cf6f9819d4dda1a719adcba58238fc9416814fd9
Andrew Huth <andrew@huth.me>
2021-05-31 13:39:40 -0700

    Change tests to more of a BDD style
    I think this will make more sense when implementing more complicated instructions.

--- src/emulator/Interpreter.test.ts
+++ src/emulator/Interpreter.test.ts
@@ -2,31 +2,35 @@ import * as Register from './Register';
 import * as Interpreter from './Interpreter';
 
 describe('instructions', () => {
-  test('1nnn - JP', () => {
-    const interpreter = Interpreter.create();
-    Interpreter.load(interpreter, [
-      // Jump to address 666.
-      0x16, 0x66,
-    ]);
+  describe('1nnn - JP', () => {
+    it('jumps to location nnn', () => {
+      const interpreter = Interpreter.create();
+      Interpreter.load(interpreter, [
+        // Jump to address 666.
+        0x16, 0x66,
+      ]);
 
-    expect(Register.get(interpreter.program_counter)).toEqual(0x200);
-    Interpreter.tick(interpreter);
-    expect(Register.get(interpreter.program_counter)).toEqual(0x666);
+      expect(Register.get(interpreter.program_counter)).toEqual(0x200);
+      Interpreter.tick(interpreter);
+      expect(Register.get(interpreter.program_counter)).toEqual(0x666);
+    });
   });
 
-  test('2nnn - CALL', () => {
-    const interpreter = Interpreter.create();
-    Interpreter.load(interpreter, [
-      // Call subroutine at address 345.
-      0x23, 0x45,
-    ]);
+  describe('2nnn - CALL', () => {
+    it('calls subroutine at address nnn', () => {
+      const interpreter = Interpreter.create();
+      Interpreter.load(interpreter, [
+        // Call subroutine at address 345.
+        0x23, 0x45,
+      ]);
 
-    expect(Register.get(interpreter.program_counter)).toEqual(0x200);
-    expect(interpreter.stack).toEqual([]);
+      expect(Register.get(interpreter.program_counter)).toEqual(0x200);
+      expect(interpreter.stack).toEqual([]);
 
-    Interpreter.tick(interpreter);
+      Interpreter.tick(interpreter);
 
-    expect(Register.get(interpreter.program_counter)).toEqual(0x345);
-    expect(interpreter.stack).toEqual([0x200]);
+      expect(Register.get(interpreter.program_counter)).toEqual(0x345);
+      expect(interpreter.stack).toEqual([0x200]);
+    });
   });
 });
