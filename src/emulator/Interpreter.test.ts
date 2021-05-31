1bd69ff5f1f96f00e84fd2ff9694098624563159
Andrew Huth <andrew@huth.me>
2021-05-31 12:25:15 -0700

    Add "describe" to the interpreter tests for instructions

--- src/emulator/Interpreter.test.ts
+++ src/emulator/Interpreter.test.ts
@@ -1,14 +1,16 @@
 import * as Register from './Register';
 import * as Interpreter from './Interpreter';
 
-test('JP', () => {
-  const interpreter = Interpreter.create();
-  Interpreter.load(interpreter, [
-    // Jump to address 666.
-    0x16, 0x66,
-  ]);
+describe('instructions', () => {
+  test('JP', () => {
+    const interpreter = Interpreter.create();
+    Interpreter.load(interpreter, [
+      // Jump to address 666.
+      0x16, 0x66,
+    ]);
 
-  expect(Register.get(interpreter.program_counter)).toEqual(0x200);
-  Interpreter.tick(interpreter);
-  expect(Register.get(interpreter.program_counter)).toEqual(0x666);
+    expect(Register.get(interpreter.program_counter)).toEqual(0x200);
+    Interpreter.tick(interpreter);
+    expect(Register.get(interpreter.program_counter)).toEqual(0x666);
+  });
 });
